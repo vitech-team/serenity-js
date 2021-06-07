f2e794fe848a4a9a37bcdb70bc485bdae7e15937
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2021-06-07 01:10:55 +0100

    ci(github): enable SauceLabs integration

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,3 +1,41 @@
+const { hostname } = require('os');
+const useSauceLabs = !! (process.env.SAUCE_USERNAME && process.env.SAUCE_ACCESS_KEY);
+
+function sauceLabsChrome(version) {
+    return {
+        shardTestFiles: false,
+        maxInstances: 1,
+
+        name: `Chrome ${ version }`,
+        browserName: 'chrome',
+        version,
+
+        // This is needed for ChromeDriver, as of ChromeDriver 75
+        // it is W3C by default. Protractor doesn't support this.
+        'goog:chromeOptions':{
+            w3c: false,
+        },
+
+        // This is needed with Protractor, providing the capabilities at the root of the config doesn't work when
+        // you provide a custom seleniumAddress
+        username:   process.env.SAUCE_USERNAME,
+        access_key: process.env.SAUCE_ACCESS_KEY,
+
+        // The build name in Sauce Labs
+        build: `Serenity/JS ${ process.env.GITHUB_REF || 'unknown branch' } #${ process.env.GITHUB_RUN_NUMBER } (${ hostname() })`,
+    }
+}
+
+const sauceLabsConfig = {
+    seleniumAddress: `https://ondemand.us-west-1.saucelabs.com/wd/hub/`,
+
+    multiCapabilities: [
+        sauceLabsChrome('latest'),
+        sauceLabsChrome('latest-1'),
+        sauceLabsChrome('beta'),
+    ]
+};
+
 const localChromeConfig = {
     chromeDriver: require(`chromedriver`).path,
     directConnect: true,
@@ -48,5 +86,7 @@ module.exports = {
         reporter: 'dot',
     },
 
-    ... localChromeConfig,
+    ... useSauceLabs
+        ? sauceLabsConfig
+        : localChromeConfig,
 };
